9a8b3a27928cd207e6644fc95e2fbe4cd1e58db6
Dan Hallenbeck <dan.hallenbeck@twosixlabs.com>
2020-05-04 09:27:06 -0500

    Initial commit for release of PE for Android

--- annotations/src/main/java/androidx/annotation/RequiresPermission.java
+++ annotations/src/main/java/androidx/annotation/RequiresPermission.java
@@ -13,6 +13,19 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+/*
+ * This work was modified by Two Six Labs, LLC and is sponsored by a subcontract agreement with
+ * Raytheon BBN Technologies Corp. under Prime Contract No. FA8750-16-C-0006 with the Air Force
+ * Research Laboratory (AFRL).
+ *
+ * The Government has unlimited rights to use, modify, reproduce, release, perform, display, or disclose
+ * computer software or computer software documentation marked with this legend. Any reproduction of
+ * technical data, computer software, or portions thereof marked with this legend must also reproduce
+ * this marking.
+ *
+ * Copyright (C) 2020 Two Six Labs, LLC.  All rights reserved.
+ */
 package androidx.annotation;
 
 import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
@@ -21,6 +34,7 @@ import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.RetentionPolicy.CLASS;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
@@ -73,7 +87,7 @@ import java.lang.annotation.Target;
  *   public static final String ACTION_CALL = "android.intent.action.CALL";
  * </code></pre>
  */
-@Retention(CLASS)
+@Retention(RUNTIME)
 @Target({ANNOTATION_TYPE,METHOD,CONSTRUCTOR,FIELD,PARAMETER})
 public @interface RequiresPermission {
     /**
